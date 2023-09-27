5fefcfe5a8d225e6004e66e7ce1cb02a7d3e6f59
ungrome <ungarov.roman@gmail.com>
2023-09-27 11:31:27 +0300

    fix App.java (help output) add Makefile

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,23 @@
 package hexlet.code;
-
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+import java.util.concurrent.Callable;
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
+            description = "Compares two configuration files and shows a difference.")
+class App implements Callable<Integer> {
+    @Option(names = {"-h", "--help"}, description = "Show this help message and exit.")
+    private String help;
+    @Option(names = {"-v", "--version"}, description = "Print version information and exit.")
+    private String version;
+    @Override
+    public Integer call() throws Exception {
+        return null;
     }
-
+    public static void main(String... args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+    }
 }
+
